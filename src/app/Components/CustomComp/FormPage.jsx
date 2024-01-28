8198cbc84deb5db322481613f298af3e117df048
Your Name <you@example.com>
2024-01-28 13:11:58 +0530

    form ui is done

---
+++ src/app/Components/CustomComp/FormPage.jsx
@@ -0,0 +1,24 @@
+import { Grid, Paper } from '@mui/material'
+import React from 'react'
+
+function FormPage({ children }) {
+    const backgroundImageUrl = 'sample.jpg'
+    return (
+        <div>
+            <Grid container justifyContent="center">
+                <Grid item md={5} xs={0} sx={{ backgroundImage: `url(${backgroundImageUrl})`, backgroundSize: "contain", backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
+
+
+                </Grid>
+                <Grid item md={5} xs={12}>
+
+                    {children}
+
+
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default FormPage
