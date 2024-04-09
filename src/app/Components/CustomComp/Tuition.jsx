3ad8403a783372971545869d873e844daef958f8
Your Name <you@example.com>
2024-04-09 19:07:44 +0530

    tution css changed

--- src/app/Components/CustomComp/Tuition.jsx
+++ src/app/Components/CustomComp/Tuition.jsx
@@ -12,57 +12,54 @@ function Tuition({ imgUrl, name, description, course, subject, date, location, v
         <div className={styles.main}>
             <div className={styles.tuition}>
                 <div className={styles.head}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={2} sm={2} md={2}>
+                    <div className="row">
+                        <div className="col-2">
                             <div className={styles.pic}>
                                 <img src={imgUrl} alt="" />
                             </div>
-                        </Grid>
 
-                        <Grid item xs={8} sm={8} md={8}>
+                        </div>
+                        <div className="col-10">
                             <div className={styles.name}>
                                 <h4>{name}</h4>
                             </div>
                             <div className={styles.desc}>
                                 <p>{description}</p>
                             </div>
-                        </Grid>
-                    </Grid>
+                        </div>
+                    </div>
                 </div>
                 <div className={styles.mid}>
-                    <Grid container spacing={2} alignContent={'flex-start'}>
-                        <Grid item >
-                            <div >
-                                <p className={styles.tag}><IconButton size='small'><SchoolOutlinedIcon /></IconButton>{course} </p>
-                            </div>
-                        </Grid>
+                    <div className="row justify-content-start">
+                        <div className="col">
 
-                        <Grid item>
-                            <div className={styles.tag}>
-                                <p><IconButton size='small'><MenuBookIcon /></IconButton>{subject}</p>
-                            </div>
-                        </Grid>
-                    </Grid>
+                            <p className={styles.tag}><IconButton size='small'><SchoolOutlinedIcon /></IconButton>{course} </p>
+                            <p className={styles.tag}><IconButton size='small'><MenuBookIcon /></IconButton>{subject}</p>
+
+                        </div>
+                    </div>
                 </div>
                 <div className={styles.bottom}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={6} sm={6} md={6}>
-                            <p><IconButton size='small'><CalendarMonthOutlinedIcon /> </IconButton>Posted Date:{date}</p>
-                        </Grid>
-                        <Grid item xs={6} sm={6} md={6}>
+                    <div className="row">
+                        <div className="col-6">
+                            <p><IconButton size='small'><CalendarMonthOutlinedIcon /> </IconButton>{date}</p>
+
+                        </div>
+                        <div className="col-6">
                             <p><IconButton size='small'><LocationOnOutlinedIcon /> </IconButton>{location}</p>
-                        </Grid>
-                    </Grid>
+                        </div>
+                    </div>
                 </div>
                 <div className={styles.bottom}>
-                    <Grid container spacing={2} justifyContent={'flex-end'} alignItems={'center'}>
-                        <Grid item xs={9} sm={9} md={9}>
+                    <div className="row">
+                        <div className="col-9">
                             {verify == true && <p><IconButton size='small' color='primary'><VerifiedOutlinedIcon /></IconButton>Tuition Verified</p>}
-                        </Grid>
-                        <Grid item xs={3} sm={3} md={3}  >
+                        </div>
+                        <div className="col-3">
                             <Anchor link={link}>Details</Anchor>
-                        </Grid>
-                    </Grid>
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
